461a8d3d95f1f1e6a74e06a4c8074b7d68d9ed95
frpourtousavecsabir-byte <fr.pour.tous.avec.sabir@gmail.com>
2026-08-28 02:27:51 +0100

    hh

--- Js/FlashCards.js
+++ Js/FlashCards.js
@@ -54,7 +54,11 @@
  
  console.log(i);
  
+ const delet = document.getElementById('delete');
  afficher.addEventListener('click', () => {
+  delet.innerHTML = '<svg class="poubelle" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#8B1A10"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg> <p>Delete</p>';
+  delet.className = 'Delete';
+
    wordToMemorise.textContent ='';
   document.querySelector('.que-signifie').innerHTML = '';
   afficher.textContent ='';
@@ -129,10 +133,20 @@
         
         
       })
+/**
+      delet.addEventListener('click', ()=> {
+        vocab.splice(i, 1);
+        console.log(vocab);
+        motSuivant();
+      });
+      console.log(vocab);
+      **/
       
       
       function motSuivant () {
         i++;
+        delet.innerHTML = '';
+        delet.classList.remove('Delete');
         console.log(i);
         localStorage.setItem('index', i);
         numeroMot.textContent++;
