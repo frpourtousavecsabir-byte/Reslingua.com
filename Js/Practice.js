48aee8d436bb6c2b995f104bb89094a6a640fb25
frpourtousavecsabir-byte <fr.pour.tous.avec.sabir@gmail.com>
2026-09-22 16:11:33 +0000

    push

--- Js/Practice.js
+++ Js/Practice.js
@@ -1,14 +1,21 @@
+
+
+
 const wordsNum = JSON.parse(localStorage.getItem('vocabToReviewUniqueWord')).length;
 
 
-let cartes = JSON.parse(localStorage.getItem('cartes')) ||  [];
+let cartes = JSON.parse(localStorage.getItem('cartes')).reverse() ||  [];
 console.log(cartes);
+
 
 const cartesToAdd = document.getElementById('cartesToAdd');
 
 
-cartes.forEach((element, index) => {
 
+
+
+cartes.forEach((element, index) => {
+       
       const allCart = document.createElement('div');
       allCart.className = 'allCart'
       allCart.innerHTML = `<div class="div" style="margin-top:1rem">
@@ -16,7 +23,7 @@ cartes.forEach((element, index) => {
       <div id="threePointCart"></div>
       <div class="word-auto">
       <span class="myWords">${element.cartName}</span>
-      <p class="word-auto"><span class="auto">Personal</span><svg id="threePoint" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#6a5417"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></p>
+      <p class="word-auto"><span class="auto">Personal</span><svg class='js-threePoint' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#6a5417"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></p>
       </div>
       <p class="sub">${element.Description}</p>
            </div>
@@ -34,19 +41,48 @@ cartes.forEach((element, index) => {
               <span class="word3" >Mastered</span>
               </div>
               </div>
-              <a class="parcticeAdd" id="start"><button class="commencerPersonal"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff9e9"><path d="m480-336 128-184H494l80-280H360v320h120v144ZM400-80v-320H280v-480h400l-80 280h160L400-80Zm80-400H360h120Z"/></svg> Practice</button ><button class="commencerPersonal"> <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff9e9"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg><span>add word</button></span></a>
+              
+              <div class="parcticeAdd" id="start">
+
+              <a class="commencerPersonal">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff9e9"><path d="m480-336 128-184H494l80-280H360v320h120v144ZM400-80v-320H280v-480h400l-80 280h160L400-80Zm80-400H360h120Z"/></svg>
+               <span>Practice</span>
+              </a>
+              
+              <button class="commencerPersonal js-commencerPersonal">
+               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff9e9"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
+              <span>add word</span>
+              </button>
               </div>`
               
               cartesToAdd.appendChild(allCart);
-              const threePoint = document.getElementById('threePoint');
-      threePoint.onclick = () => {
-        cartes.splice(0, 1);
-        localStorage.setItem('cartes', JSON.stringify(cartes));
-        location.reload();
-      };
-    });
-    
 
+              
+            });
+            const threePoint = document.querySelectorAll('.js-threePoint');
+            threePoint.forEach((element, index) => {
+
+                element.addEventListener('click', () => {
+                  localStorage.setItem('index', index);
+                  cartes.splice(index, 1);
+                  console.log(cartes);
+                  localStorage.setItem('cartes', JSON.stringify(cartes));
+                   
+                  //location.reload();
+
+                }) 
+            });
+    const addd =  document.querySelectorAll('.js-commencerPersonal');
+    addd.forEach((element, index) => {
+
+                element.addEventListener('click', () => {
+                  localStorage.setItem('index', index);
+                  
+                  console.log(index);
+                  window.location.href = 'addWordsToCart.html';
+                }) 
+            });
+     
 
 
 const wordsNumId = document.getElementById('wordsNum');
@@ -74,6 +110,7 @@ commencer.addEventListener('click', ()=> {
      const cancel = document.createElement('button');
      const groupVocab = document.createElement('div');
      const vocaName = document.createElement('div');
+     const vide = document.createElement('div');
      const vocaDescription = document.createElement('div');
      
      
@@ -103,14 +140,12 @@ commencer.addEventListener('click', ()=> {
         cancel.className = 'cancel';
         cancel.innerHTML = '✘'
         
+        groupVocab.className = 'groupVocab'
+        
         
         vocaName.innerHTML= `<input id="vocaNameInput" class="vocaNameInput1" placeHolder="Name">`;
-        const vocaNameInput = document.getElementById('vocaNameInput');
-
+        
         vocaDescription.innerHTML= `<input id="vocaDescriptionInput" class="vocaNameInput2" placeHolder="Description">`;
-        const vocaDescriptionInput = document.getElementById('vocaDescriptionInput');
-        
-        
         
         contCart.appendChild(page);
         mainBlurr.appendChild(contBlurr);
@@ -120,8 +155,11 @@ commencer.addEventListener('click', ()=> {
         doneCancel.appendChild(done);
         page.appendChild(groupVocab);
         groupVocab.appendChild(vocaName);
+        groupVocab.appendChild(vide);
         groupVocab.appendChild(vocaDescription);
         
+        const vocaNameInput = document.getElementById('vocaNameInput');
+        const vocaDescriptionInput = document.getElementById('vocaDescriptionInput');
         
         cancel.onclick = ()=> {
           localStorage.removeItem('add')
@@ -129,29 +167,35 @@ commencer.addEventListener('click', ()=> {
           contCart.remove();
           add.className = 'add';
           add.textContent = '+'
-       };
-
-     
-       done.addEventListener('click', ()=> {
-       
-          cartes = JSON.parse(localStorage.getItem('cartes')) ||  [];
-             localStorage.removeItem('add')
-             contBlurr.remove();
-              contCart.remove();
-              add.className = 'add';
-           add.textContent = '+';
-           let cartObject = {};
-           cartObject.cartName = `${vocaNameInput.value}`;
+        };
+        
+        
+        
+      done.onclick = ()=> {
+        if(vocaNameInput.value.trim() === ''.trim()) {
+         console.log('hello');
+        vocaNameInput.classList.add('vocaNameAfter1');
+         vide.className= 'vide';
+         vide.textContent= 'The name can not be empty!'
+         return  cartes;
+        } else {
+          localStorage.removeItem('add')
+          contBlurr.remove();
+          contCart.remove();
+          add.className = 'add';
+         add.textContent = '+';
+         let cartObject = {};
+          cartObject.cartName = `${vocaNameInput.value}`;
            cartObject.Description = `${vocaDescriptionInput.value}`;
+           cartObject.wordMeaningCarts = [];
            cartes.push(cartObject);
            console.log(cartes);
            localStorage.setItem('cartes', JSON.stringify(cartes));
            location.reload();
 
         }
-
-         )
-      };
+      }
+    };
     
     add.addEventListener('click', () => {
        cartes = JSON.parse(localStorage.getItem('cartes'));
